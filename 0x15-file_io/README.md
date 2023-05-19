6188f4713bb0cb6652ea02f1ffe24c7c1d18cc66
Gharddammez <guavgharddammez@gmail.com>
2023-05-19 12:29:14 -0700

    alx-low_level_programming

--- 0x15-file_io/README.md
+++
@@ -1,1 +0,0 @@
-file tasks  tag 0x15
--- 0x15-file_io/main.h
+++
@@ -1,4 +0,0 @@
-int my_putchar(char character);
-ssize_t my_read_textfile(const char *filename, size_t letters);
-int my_create_file(const char *filename, char *text_content);
-int my_append_text_to_file(const char *filename, char *text_content);
