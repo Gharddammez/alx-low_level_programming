193eaf2c4af0f7b43f396e07f21857fbd70d3eed
Gharddammez <guavgharddammez@gmail.com>
2023-08-03 13:40:56 -0700

    Add password for crackme3

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,36 +1,30 @@
 #include "main.h"
 #include <stdio.h>
-#include <string.h>
 
 /**
- * binary_to_uint - Converts a binary number to an unsigned integer.
- * @binary: Pointer to a string of characters.
- *
- * Return: The converted number, or 0 if a character in the string is not '0' or '1'
- * or if the string is NULL.
+ * binary_to_uint - convert a binary number to an unsigned int
+ * @b: char string
+ * Return: converted decimal number or 0 if there is an unconvertable char
  */
-unsigned int binary_to_uint(const char *binary)
+unsigned int binary_to_uint(const char *b)
 {
-    int len, i;
-    unsigned int result = 0;
-    unsigned int decimalValue = 1;
+	unsigned int total, power;
+	int len;
 
-    if (binary == NULL)
-        return 0;
+	if (b == NULL)
+		return (0);
 
-    len = strlen(binary);
+	for (len = 0; b[len]; len++)
+	{
+		if (b[len] != '0' && b[len] != '1')
+			return (0);
+	}
 
-    for (i = (len - 1); i >= 0; i--)
-    {
-        if (binary[i] != '0' && binary[i] != '1')
-            return 0;
+	for (power = 1, total = 0, len--; len >= 0; len--, power *= 2)
+	{
+		if (b[len] == '1')
+			total += power;
+	}
 
-        if (binary[i] == '1')
-            result += decimalValue;
-
-        decimalValue *= 2;
-    }
-
-    return result;
+	return (total);
 }
-
