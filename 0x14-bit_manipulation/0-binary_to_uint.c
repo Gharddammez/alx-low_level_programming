9ddfb41213fa6eed2e4a2cf8c50ac66dfc930677
Gharddammez <guavgharddammez@gmail.com>
2023-08-03 13:19:49 -0700

    Fix binary_to_uint function

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -13,21 +13,22 @@ unsigned int binary_to_uint(const char *binary)
 {
     int len, i;
     unsigned int result = 0;
+    unsigned int decimalValue = 1;
 
     if (binary == NULL)
         return 0;
 
     len = strlen(binary);
 
-    for (i = 0; i < len; i++)
+    for (i = (len - 1); i >= 0; i--)
     {
         if (binary[i] != '0' && binary[i] != '1')
             return 0;
 
-        result <<= 1; // Left-shift the result by 1 to make space for the next bit
-
         if (binary[i] == '1')
-            result |= 1; // Set the least significant bit to 1
+            result += decimalValue;
+
+        decimalValue *= 2;
     }
 
     return result;
