9681eadb5ae2dd01f19b511ce5989a2eacb8c7be
Gharddammez <guavgharddammez@gmail.com>
2023-08-03 13:11:22 -0700

    Fix binary_to_uint function

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -3,39 +3,34 @@
 #include <string.h>
 
 /**
- * binary_to_uint - converts a binary number to an unsigned integer
- * @binary: pointer to a string of characters
+ * binary_to_uint - Converts a binary number to an unsigned integer.
+ * @binary: Pointer to a string of characters.
  *
- * Return: The converted number, or 0 if character in string is not 1
- * or 0 or if string is NULL
+ * Return: The converted number, or 0 if a character in the string is not '0' or '1'
+ * or if the string is NULL.
  */
 unsigned int binary_to_uint(const char *binary)
 {
-	int len = strlen(binary);
-	unsigned int result = 0;
-	int decimalValue = 1, i;
+    int len, i;
+    unsigned int result = 0;
+    unsigned int decimalValue = 1;
 
-	if (binary == NULL)
-	{
-		return (0);
-	}
-	for (len = 0; binary[len]; len++)
-	{
-		if (binary[len] != '0' && binary[len] != '1')
-		{
-			return (0);
-		}
-	}
+    if (binary == NULL)
+        return 0;
 
-	for (i = (len - 1); i >= 0; i--)
-	{
-		if (binary[i] == '1')
-		{
-			result += decimalValue;
-			decimalValue *= 2;
-		}
-	}
+    len = strlen(binary);
 
-	return (result);
+    for (i = (len - 1); i >= 0; i--)
+    {
+        if (binary[i] != '0' && binary[i] != '1')
+            return 0;
+
+        if (binary[i] == '1')
+            result += decimalValue;
+
+        decimalValue *= 2;
+    }
+
+    return result;
 }
 
